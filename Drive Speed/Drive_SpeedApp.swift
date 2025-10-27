4ba296c52dd2954d59fcbc17bf46d3e8457f315d
George Babichev <gbabichev@xantrion.com>
2025-10-27 11:02:24 -0400

    UX updates

--- Drive Speed/Drive_SpeedApp.swift
+++ Drive Speed/Drive_SpeedApp.swift
@@ -9,10 +9,25 @@ import SwiftUI
 
 @main
 struct Drive_SpeedApp: App {
+    @State private var showAbout = false
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ZStack {
+                ContentView()
+
+                if showAbout {
+                    AboutView(isPresented: $showAbout)
+                }
+            }
         }
         .windowStyle(.hiddenTitleBar)
+        .commands {
+            CommandGroup(replacing: .appInfo) {
+                Button("About Drive Speed") {
+                    showAbout = true
+                }
+            }
+        }
     }
 }
