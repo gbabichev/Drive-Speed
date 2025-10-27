b08e08d123d06466fd58a89dff77ffc31a069ca4
George Babichev <gbabichev@xantrion.com>
2025-10-27 09:45:34 -0400

    Initial Commit

---
+++ Drive Speed/Drive_SpeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drive_SpeedApp.swift
+//  Drive Speed
+//
+//  Created by George Babichev on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Drive_SpeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
